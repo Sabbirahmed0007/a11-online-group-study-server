9691e56887cb0eb9f5d36de4a438b01791714763
Sabbirahmed0007 <>
2023-11-08 21:38:03 +0600

    created a jwt path and verifytoken

--- index.js
+++ index.js
@@ -31,6 +31,32 @@ const client = new MongoClient(uri, {
   }
 });
 
+/// jwt Middlewares
+
+
+ const gateMan = (req, res, next)=>{
+    console.log('Called: ', req.method, req.url, req.hostname, req.originalUrl);
+    next()
+ }
+
+ const verifyToken=(req, res, next)=>{
+    const token = req.cookies?.token;
+    console.log('Token in the middle ware', token);
+    if(!token){
+        return res.status(401).send({message: 'Unauthorized Uses'});
+
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+        if(err){
+            return res.status(401).send({message: 'Unauthorized user access'})
+        }
+        req.user= decoded;
+        next()
+    })
+    
+
+ }
+
 
 
 
@@ -44,17 +70,27 @@ async function run() {
 
     // Auth jwt related API
 
+
+    // jwt token
     app.post('/jwt',  async(req, res)=>{
         const user= req.body;
-        console.log(user);
+        // console.log(user);
         const token=jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2hr' })
-        console.log(token)
+        // console.log(token)
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production', 
             sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
             
         }).send({success: true})
+    })
+
+
+    /// jwt  logout and erase token
+    app.post('/logout', async(req, res)=>{
+        const user = req.body;
+        console.log('logged out', user)
+        res.clearCookie('token', {maxAge: 0 }).send({success: true})
     })
 
    
